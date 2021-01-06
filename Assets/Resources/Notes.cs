da31ef7a7c7a781175b924946d6c46578b795841
plowiecm <plowiecmagdalena@gmail.com>
2021-01-06 01:27:29 +0100

    add note panel - name of target and text

---
+++ Assets/Resources/Notes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddNewNote()
+    {
+        TMPro.TMP_InputField myInputField = GameObject.Find("InputFieldMakeNewNote").GetComponent<TMPro.TMP_InputField>();
+        string textOfField = myInputField.text;
+
+        IEnumerable<Vuforia.TrackableBehaviour> tbs = Vuforia.TrackerManager.Instance.GetStateManager().GetActiveTrackableBehaviours();
+        var tb = tbs.FirstOrDefault();
+
+        if (tb != null && !string.IsNullOrEmpty(textOfField))
+        {
+            Debug.Log("Note for imageTarget: " + tb.name + " added: " + textOfField);
+            //todomp send note by api
+        }
+        myInputField.text = "";
+    }
+}
